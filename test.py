b1c310e0e7e1fd2ed9f00e53753d27c494989be4
justusnoe <justus.noegel@posteo.de>
2023-10-27 15:19:16 +0100

    changes

--- test.py
+++ test.py
@@ -5,12 +5,15 @@ def mysqrt(x):
     xmax = x
     tol = 0.000001
     mid = (xmin+xmax)/2
-    while abs(mid*mid-x)>tol:
-        if mid*mid>x:
-            xmax=mid
-        else: 
-            xmin=mid 
-        mid = (xmin+xmax)/2
-    return mid
+    if x >= 0:
+        while abs(mid*mid-x)>tol:
+            if mid*mid>x:
+                xmax=mid
+            else: 
+                xmin=mid 
+            mid = (xmin+xmax)/2
+        return mid
+    else:
+        print("NaN")
 
-print(mysqrt(9))
+print(mysqrt(-1))
